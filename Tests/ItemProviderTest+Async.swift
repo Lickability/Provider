fac0a9c671f6da8be9dc878a83b3037b4326c1ec
SPRSLEPR <ashlirankin18@gmail.com>
2025-06-30 16:10:30 -0400

    Added tests
    Signed-off-by: SPRSLEPR <ashlirankin18@gmail.com>

--- Tests/ItemProviderTest+Async.swift
+++ Tests/ItemProviderTest+Async.swift
@@ -243,4 +243,143 @@ final class ItemProviderTests_Async: XCTestCase {
             XCTFail("An unexpected error occurred: \(error)")
         }
     }
+    
+    // MARK: - Async Provide Items Async Stream Tests
+    
+    func testProvideItemsStream() async {
+        let request = TestProviderRequest()
+        
+        stub(condition: { _ in true }) { _ in
+            fixture(filePath: self.itemsPath, headers: nil)
+        }
+        
+        let testItemResult: AsyncStream<Result<[TestItem], ProviderError>> = await provider.asyncProvideItems(request: request, decoder: JSONDecoder(), providerBehaviors: [], requestBehaviors: [])
+        for try await result in testItemResult {
+            switch result {
+            case let .success(testItems):
+                XCTAssertEqual(testItems.count, 3)
+            case let .failure(error):
+                XCTFail("There should be no error: \(error)")
+            }
+          
+        }
+    }
+    
+    func testProvideItemsDoesNotReturnPartialResponseUponFailureForExpiredItemsStream() async {
+        let request = TestProviderRequest()
+        
+        let originalStub = stub(condition: { _ in true }) { _ in
+            fixture(filePath: self.itemsPath, headers: nil)
+        }
+        
+        let _ : AsyncStream<Result<[TestItem], ProviderError>> = await expiredProvider.asyncProvideItems(request: request)
+        
+        try? self.expiredProvider.cache?.remove(forKey: "Hello 2")
+        HTTPStubs.removeStub(originalStub)
+        
+        stub(condition: { _ in true}) { _ in
+            fixture(filePath: self.itemPath, headers: nil)
+        }
+        
+        let expiredResult : AsyncStream<Result<[TestItem], ProviderError>> = await expiredProvider.asyncProvideItems(request: request)
+        
+        for await result in expiredResult {
+            switch result {
+            case .success:
+                XCTFail("Should have received a decoding error.")
+            case let .failure(error):
+                switch error {
+                case .decodingError: break
+                default: XCTFail("Should have received a decoding error.")
+                }
+            }
+        }
+    }
+    
+    func testProvideItemsFailureStream() async {
+        let request = TestProviderRequest()
+        
+        stub(condition: { _ in true }) { _ in
+            fixture(filePath: OHPathForFile("InvalidItems.json", type(of: self))!, headers: nil)
+        }
+        
+        let result : AsyncStream<Result<[TestItem], ProviderError>> = await provider.asyncProvideItems(request: request)
+        for await result in result {
+            switch result {
+            case .success:
+                XCTFail("There should be an error.")
+            case .failure: break
+            }
+        }
+    }
+    
+    // MARK: - Async Provide Item Async Stream Tests
+    
+    func testProvideItemStream() async {
+        let request = TestProviderRequest()
+        
+        stub(condition: { _ in true }) { _ in
+            fixture(filePath: self.itemPath, headers: nil)
+        }
+        
+        let result: AsyncStream<Result<TestItem, ProviderError>> = await provider.asyncProvide(request: request)
+        
+        for await result in result {
+            switch result {
+            case .success: break
+            case let .failure(error):
+                XCTFail("There should be no error: \(error)")
+            }
+        }
+    }
+    
+    func testProvideItemReturnsCachedResultStream() async {
+        let request = TestProviderRequest()
+        
+        let originalStub = stub(condition: { _ in true }) { _ in
+            fixture(filePath: self.itemPath, headers: nil)
+        }
+        
+        let result: Result<TestItem, ProviderError> = await provider.asyncProvide(request: request)
+        
+        switch result {
+        case .success:
+            HTTPStubs.removeStub(originalStub)
+            
+            stub(condition: { _ in true }) { _ in
+                fixture(filePath: OHPathForFile("InvalidItem.json", type(of: self))!, headers: nil)
+            }
+            
+            let result: AsyncStream<Result<TestItem, ProviderError>> = await provider.asyncProvide(request: request)
+            for await result in result {
+                switch result {
+                case .success:
+                    break
+                case let .failure(error):
+                    XCTFail("There should be no error: \(error)")
+                }
+            }
+            
+        case let .failure(error):
+            XCTFail("There should be no error: \(error)")
+        }
+    }
+    
+    func testProvideItemFailureStream() async {
+        let request = TestProviderRequest()
+        
+        stub(condition: { _ in true }) { _ in
+            fixture(filePath: OHPathForFile("InvalidItem.json", type(of: self))!, headers: nil)
+        }
+        
+        let result: AsyncStream<Result<TestItem, ProviderError>> = await provider.asyncProvide(request: request)
+        
+        for await result in result {
+            switch result {
+            case .success:
+                XCTFail("There should be an error.")
+            case .failure: break
+            }
+        }
+    }
 }
