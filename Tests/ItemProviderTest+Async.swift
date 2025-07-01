07f4c29bcb9298f116ebecba303a5c990296a59f
SPRSLEPR <ashlirankin18@gmail.com>
2025-07-01 11:44:45 -0400

    Updated tests
    Signed-off-by: SPRSLEPR <ashlirankin18@gmail.com>

--- Tests/ItemProviderTest+Async.swift
+++ Tests/ItemProviderTest+Async.swift
@@ -39,212 +39,6 @@ final class ItemProviderTests_Async: XCTestCase {
         try? expiredProvider.cache?.removeAll()
     }
     
-    // MARK: - Async Provide Items Tests
-    
-    func testProvideItems() async {
-        let request = TestProviderRequest()
-        
-        stub(condition: { _ in true }) { _ in
-            fixture(filePath: self.itemsPath, headers: nil)
-        }
-        
-        let result: Result<[TestItem], ProviderError> = await provider.asyncProvideItems(request: request)
-        
-        switch result {
-        case let .success(items):
-            XCTAssertEqual(items.count, 3)
-        case let .failure(error):
-            XCTFail("There should be no error: \(error)")
-        }
-    }
-    
-    func testProvideItemsReturnsPartialResponseUponFailure() async {
-        let request = TestProviderRequest()
-        
-        let originalStub = stub(condition: { _ in true }) { _ in
-            fixture(filePath: self.itemsPath, headers: nil)
-        }
-
-        let _ : Result<[TestItem], ProviderError> = await provider.asyncProvideItems(request: request)
-        
-        try? self.provider.cache?.remove(forKey: "Hello 2")
-        HTTPStubs.removeStub(originalStub)
-        
-        stub(condition: { _ in true}) { _ in
-            fixture(filePath: self.itemPath, headers: nil)
-        }
-        
-        let secondResult: Result<[TestItem], ProviderError> = await provider.asyncProvideItems(request: request)
-       
-        switch secondResult {
-        case .success:
-            XCTFail("Should have received a partial retrieval failure.")
-        case let .failure(error):
-            switch error {
-            case let .partialRetrieval(retrievedItems, persistenceErrors, error):
-                let expectedItemIDs = ["Hello 1", "Hello 3"]
-                
-                XCTAssertEqual(retrievedItems.map { $0.identifier }, expectedItemIDs)
-                XCTAssertEqual(persistenceErrors.count, 1)
-                XCTAssertEqual(persistenceErrors.first?.key, "Hello 2")
-                
-                guard case ProviderError.decodingError = error else {
-                    XCTFail("Incorrect error received.")
-                    return
-                }
-                
-                guard let persistenceError = persistenceErrors.first?.persistenceError, case PersistenceError.noValidDataForKey = persistenceError else {
-                    XCTFail("Incorrect error received.")
-                    return
-                }
-                
-            default: XCTFail("Should have received a partial retrieval error.")
-            }
-        }
-    }
-    
-    func testProvideItemsDoesNotReturnPartialResponseUponFailureForExpiredItems() async {
-        let request = TestProviderRequest()
-        
-        let originalStub = stub(condition: { _ in true }) { _ in
-            fixture(filePath: self.itemsPath, headers: nil)
-        }
-        
-        let _ : Result<[TestItem], ProviderError> = await expiredProvider.asyncProvideItems(request: request)
-        
-        try? self.expiredProvider.cache?.remove(forKey: "Hello 2")
-        HTTPStubs.removeStub(originalStub)
-        
-        stub(condition: { _ in true}) { _ in
-            fixture(filePath: self.itemPath, headers: nil)
-        }
-        
-        let expiredResult : Result<[TestItem], ProviderError> = await expiredProvider.asyncProvideItems(request: request)
-        
-        switch expiredResult {
-        case .success:
-            XCTFail("Should have received a decoding error.")
-        case let .failure(error):
-            switch error {
-            case .decodingError: break
-            default: XCTFail("Should have received a decoding error.")
-            }
-        }
-    }
-    
-    func testProvideItemsFailure() async {
-        let request = TestProviderRequest()
-        
-        stub(condition: { _ in true }) { _ in
-            fixture(filePath: OHPathForFile("InvalidItems.json", type(of: self))!, headers: nil)
-        }
-        
-        let result : Result<[TestItem], ProviderError> = await provider.asyncProvideItems(request: request)
-        switch result {
-        case .success:
-            XCTFail("There should be an error.")
-        case .failure: break
-        }
-    }
-    
-    // MARK: - Async Provide Item Tests
-    
-    func testProvideItem() async {
-        let request = TestProviderRequest()
-        
-        stub(condition: { _ in true }) { _ in
-            fixture(filePath: self.itemPath, headers: nil)
-        }
-        
-        let result: Result<TestItem, ProviderError> = await provider.asyncProvide(request: request)
-        
-        switch result {
-        case .success: break
-        case let .failure(error):
-            XCTFail("There should be no error: \(error)")
-        }
-    }
-    
-    func testProvideItemReturnsCachedResult() async {
-        let request = TestProviderRequest()
-        
-        let originalStub = stub(condition: { _ in true }) { _ in
-            fixture(filePath: self.itemPath, headers: nil)
-        }
-        
-        let result: Result<TestItem, ProviderError> = await provider.asyncProvide(request: request)
-        
-        switch result {
-        case .success:
-            HTTPStubs.removeStub(originalStub)
-            
-            stub(condition: { _ in true }) { _ in
-                fixture(filePath: OHPathForFile("InvalidItem.json", type(of: self))!, headers: nil)
-            }
-            
-            let result: Result<TestItem, ProviderError> = await provider.asyncProvide(request: request)
-            switch result {
-            case .success:
-                break
-            case let .failure(error):
-                XCTFail("There should be no error: \(error)")
-            }
-            
-        case let .failure(error):
-            XCTFail("There should be no error: \(error)")
-        }
-    }
-    
-    func testProvideItemFailure() async {
-        let request = TestProviderRequest()
-    
-        stub(condition: { _ in true }) { _ in
-            fixture(filePath: OHPathForFile("InvalidItem.json", type(of: self))!, headers: nil)
-        }
-        
-        let result: Result<TestItem, ProviderError> = await provider.asyncProvide(request: request)
-        switch result {
-        case .success:
-            XCTFail("There should be an error.")
-        case .failure: break
-        }
-    }
-    
-    func testAsyncProvideItemsWithCustomDecoder() async {
-        let request = TestProviderRequest()
-        
-        stub(condition: { _ in true }) { _ in
-            fixture(filePath: self.datesPath, headers: nil)
-        }
-        
-        // Test first to ensure failure when not providing a custom decoder.
-        let result1: Result<[TestDateContainer], ProviderError> = await provider.asyncProvideItems(request: request)
-        
-        switch result1 {
-        case .success:
-            XCTFail("Decoding should fail due to incorrect date format")
-        case let .failure(error):
-            switch error {
-            case .decodingError:
-                break
-            default:
-                XCTFail("An unexpected, non-decoding error occurred: \(error)")
-            }
-        }
-        
-        // Now test the same file with our custom decoder.
-        let customDecoder = JSONDecoder()
-        customDecoder.dateDecodingStrategy = .iso8601
-        let result2: Result<[TestDateContainer], ProviderError> = await provider.asyncProvideItems(request: request, decoder: customDecoder)
-        
-        switch result2 {
-        case let .success(dateContainers):
-            XCTAssertEqual(dateContainers.count, 2)
-        case let .failure(error):
-            XCTFail("An unexpected error occurred: \(error)")
-        }
-    }
-    
     // MARK: - Async Provide Items Async Stream Tests
     
     func testProvideItemsStream() async {
@@ -341,28 +135,30 @@ final class ItemProviderTests_Async: XCTestCase {
             fixture(filePath: self.itemPath, headers: nil)
         }
         
-        let result: Result<TestItem, ProviderError> = await provider.asyncProvide(request: request)
+        let result: AsyncStream<Result<TestItem, ProviderError>> = await provider.asyncProvide(request: request)
         
-        switch result {
-        case .success:
-            HTTPStubs.removeStub(originalStub)
-            
-            stub(condition: { _ in true }) { _ in
-                fixture(filePath: OHPathForFile("InvalidItem.json", type(of: self))!, headers: nil)
-            }
-            
-            let result: AsyncStream<Result<TestItem, ProviderError>> = await provider.asyncProvide(request: request)
-            for await result in result {
-                switch result {
-                case .success:
-                    break
-                case let .failure(error):
-                    XCTFail("There should be no error: \(error)")
+        for await result in result {
+            switch result {
+            case .success:
+                HTTPStubs.removeStub(originalStub)
+                
+                stub(condition: { _ in true }) { _ in
+                    fixture(filePath: OHPathForFile("InvalidItem.json", type(of: self))!, headers: nil)
                 }
+                
+                let result: AsyncStream<Result<TestItem, ProviderError>> = await provider.asyncProvide(request: request)
+                for await result in result {
+                    switch result {
+                    case .success:
+                        break
+                    case let .failure(error):
+                        XCTFail("There should be no error: \(error)")
+                    }
+                }
+                
+            case let .failure(error):
+                XCTFail("There should be no error: \(error)")
             }
-            
-        case let .failure(error):
-            XCTFail("There should be no error: \(error)")
         }
     }
     
